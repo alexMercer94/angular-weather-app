64cf83871b51a5cbf0da09d44218916ad93d3afd
Anzen Digital <alex.c.valera94@hotmail.com>
2019-02-26 16:53:23 -0600

    First commit

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  apiKey = 'a7653a205038707f13f20764a49db70a';
+  URI: string;
+
+  constructor(private http: HttpClient) {
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
+  }
+
+  getWeather(cityName: string, countryCode: string) {
+    return this.http.get(`${this.URI}${cityName},${countryCode}`);
+  }
+}
